0505755508353cf9b19661025987cabe7dbdff8d
Haripriya2408 <haripriyaveluchamy@gmail.com>
2025-01-02 11:49:34 +0530

    added docker install

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+# Install Docker if not installed
+if ! command -v docker &> /dev/null; then
+    apt-get update
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+fi
+
+# Start Docker service if not running
+systemctl start docker
+systemctl enable docker
+
 # Pull the Docker image from Docker Hub
 docker pull harivp1234/simple-python-flask-app
 
