bf1f100608bf6565a10a695d5dd5426e5c6547f7
Haripriya2408 <haripriyaveluchamy@gmail.com>
2025-01-02 11:34:29 +0530

    corrected command

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,5 +4,5 @@ set -e
 # Stop the running container (if any)
 echo "Hi hari we succeeded the project"
 
-containerid=`docker ps | awk -F " " '{print$1}'`
-docker rm-f $containerid
+containerid=`docker ps | awk -F" "'{print$1}'`
+docker rm -f $containerid
